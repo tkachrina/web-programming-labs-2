7de2b3b3da5b06937f37af9f94b0130696951008
Ткаченко Катя <tkacheks03@mail.ru>
2023-10-11 00:02:43 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ app = Flask (__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+
+<!doctype html>
+<html>
+    <head>
+        <title> Ткаченко Екатерина Игоревна, лабораторная 1 </title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h>
+
+        <footer>
+            &copy; Ткаченко Екатерина, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>    
+"""
+
