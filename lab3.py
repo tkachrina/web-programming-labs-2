6194452960aa6a75b26a60482e9e0381004eb0e3
Ткаченко Катя <tkacheks03@mail.ru>
2023-11-14 23:13:17 +0700

    заказ напитка

--- lab3.py
+++ lab3.py
@@ -13,8 +13,40 @@ def form1():
     if user == '':
         errors['user'] = 'Заполните поле!'
 
+    errors2 = {}
+    age = request.args.get('age')
+    if age == '':
+        errors2['age'] = 'Заполните поле!'
+
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+    return render_template('form1.html', user=user, age=age, sex=sex, errors=errors, errors2=errors2)
 
-    
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    user = request.args.get('user')
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, черный чай - 80 рублей, зеленый - 70 рублей.
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    # Добавление молока удорожает напиток на 30 рублей, а сахар - на 10.
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('pay.html', user=user, price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
