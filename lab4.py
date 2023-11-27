0c95bedbb1171591656a765963dbb8b29bed19ea
Ткаченко Катя <tkacheks03@mail.ru>
2023-11-27 12:38:51 +0700

    куки и заказ зерна

--- lab4.py
+++ lab4.py
@@ -43,3 +43,63 @@ def fridge():
             error = f'Температура установлена: {temperature}❄️'
     return render_template('fridge.html',temperature=temperature, error=error)  
 
+@lab4.route('/lab4/grain', methods= ['GET','POST'])
+def grain():
+    if request.method == 'GET':
+        return render_template('grain.html')
+    price = 0
+    error = ''
+    error1 = ''
+    grain = request.form.get('grain')
+    weight = request.form.get('weight')
+
+    if weight=='':
+        error = "Нужный вес не задан"
+        return render_template('grain.html', error=error)
+    weight = int(weight)
+
+    if grain == 'barley':
+        price = 12000 * weight
+    elif grain == 'oats':
+        price = 8500 * weight
+    elif grain == 'wheat':
+        price = 8700 * weight
+    else: 
+        grain == 'rye'
+        price = 14000 * weight
+    if weight <=0:
+        error = "Значение неверно"
+        return render_template('grain.html',error=error)
+    elif weight > 500:
+        error = "Нужного объема зерна в наличии нет"
+        return render_template('grain.html',error=error)
+    elif weight > 50:
+        price = price - (price * 10/100)
+        error1 = "Скидка 10% за больший объем "
+    return render_template('grains.html',error=error, price=price, grain=grain, weight=weight, error1=error1)    
+
+@lab4.route('/lab4/grains', methods= ['GET','POST'])
+def grains():
+    if request.method == 'GET':
+        return render_template('grains.html')
+    
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html')
+
+    color = request.form.get('color_text')
+    color_bg = request.form.get('color_bg')
+    fontsize = request.form.get('font_size')
+
+    headers = {
+        'Set-Cookie': [
+            'color=' + color + '; path=/',
+            'color_bg=' + color_bg + '; path=/',
+            'font_size=' + fontsize + '; path=/',
+        ],
+        'Location': '/lab4/cookies'
+    }
+
+    return '', 303, headers
+
