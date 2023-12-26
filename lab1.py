291053ad55eee163a8a7a7c925dcd631deb437f2
Ткаченко Катя <tkacheks03@mail.ru>
2023-12-26 14:14:29 +0700

    лабораторная 6

--- lab1.py
+++ lab1.py
@@ -37,7 +37,13 @@ def menu():
             </li>
             <li> 
                 <a href="/lab4/" target="_blank"> Лабораторная работа 4 </a> 
-            </li> 
+            </li>
+            <li> 
+                <a href="/lab5/" target="_blank"> Лабораторная работа 5 </a> 
+            </li>
+            <li> 
+                <a href="/lab6/" target="_blank"> Лабораторная работа 6 </a> 
+            </li>   
         </ol> 
 
         <footer style="margin-top:20px;">
